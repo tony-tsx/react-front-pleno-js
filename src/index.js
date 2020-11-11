fabe8ff5f0adc9045e66219c0ade9d72a3aa96aa
Tony <tony.js@zoho.eu>
2020-11-10 21:13:59 -0300

    fix: bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './app'
+// import MemoApp from './app/MemoApp'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
